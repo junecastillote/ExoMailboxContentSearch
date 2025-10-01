18f5ca7ff3b509d86db5da40cc633438bea11275
June Castillote <june.castillote@gmail.com>
2025-10-01 12:58:12 +0800

    Update Search-MailboxBodyContent.ps1

--- Search-MailboxBodyContent.ps1
+++ Search-MailboxBodyContent.ps1
@@ -1,16 +1,60 @@
 [CmdletBinding()]
 param (
     [Parameter(Mandatory)]
-    [string]
-    $UserId,
+    [string[]] $UserId,
 
-    # Parameter help description
     [Parameter(Mandatory)]
-    [string]
-    $SearchString
+    [string] $SearchString,
+
+    [int] $Top
 )
 
-Get-MgUserMessage -Search "body:$($searchString)" -UserId $UserId -All | Select-Object subject,
-@{n = "From"; e = { $_.from.emailAddress.address } },
-receivedDateTime,
-bodyPreview
+foreach ($id in $UserId) {
+    $searchParam = @{
+        Search   = "body:$SearchString"
+        UserId   = $id
+        Property = "subject,from,receivedDateTime,body,HasAttachments"
+    }
+
+    if ($Top) {
+        $searchParam.Top = $Top
+    }
+    else {
+        $searchParam.All = $true
+        $searchParam.PageSize = 100
+    }
+
+    try {
+        $messages = Get-MgUserMessage @searchParam -ErrorAction Stop
+    }
+    catch {
+        Write-Warning "Failed to retrieve messages for $id : $_"
+        continue
+    }
+
+    foreach ($msg in $messages) {
+        $bodyText = [System.Net.WebUtility]::HtmlDecode(
+            ($msg.Body.Content -replace '<[^>]+>', ' ')
+        )
+
+        if (-not $msg.HasAttachments -or $bodyText -match [regex]::Escape($SearchString)) {
+            $matchLocation = "Body"
+            $attachments = @()
+        }
+        else {
+            $matchLocation = "Attachment"
+            $attachments = Get-MgUserMessageAttachment -UserId $id -MessageId $msg.Id
+        }
+
+        [PSCustomObject]@{
+            Mailbox            = $id
+            Subject            = $msg.Subject
+            From               = $msg.From.EmailAddress.Address
+            ReceivedDate       = $msg.ReceivedDateTime
+            FirstMatchLocation = $matchLocation
+            MessageId          = $msg.Id
+            AttachmentNames    = ($attachments).Name -join ","
+            AttachmentIds      = ($attachments).Id -join ","
+        }
+    }
+}
